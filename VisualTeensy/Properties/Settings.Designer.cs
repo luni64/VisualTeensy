bea57af6820f1c31c6b0c341a91bcaeac836f827
luni64 <lutz.niggl@lunoptics.com>
2020-08-14 08:04:03 +0200

    Added MRU List

--- VisualTeensy/Properties/Settings.Designer.cs
+++ VisualTeensy/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace VisualTeensy.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -278,6 +278,19 @@ namespace VisualTeensy.Properties {
             }
             set {
                 this["FirstStart"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\lutz\\source\\Teensy\\inotest|C:\\Users\\lutz\\source\\Teensy\\inotest\\MultiSeri" +
+            "al")]
+        public string mruString {
+            get {
+                return ((string)(this["mruString"]));
+            }
+            set {
+                this["mruString"] = value;
             }
         }
     }
---
+++ vtCore/Implementation/MruList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace vtCore
+{
+    public class MruList
+    {
+        public List<string> projects;
+
+        public MruList(int maxProject)
+        {
+            this.maxProjects = maxProject;
+        }
+
+        public void load(string mruString)
+        {
+            projects = mruString
+                .Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(p => Directory.Exists(p))
+                .Take(maxProjects)
+                .ToList();
+        }
+
+        override public string ToString()
+        {
+            if (projects.Count == 0) return "";
+
+            StringBuilder mruString = new StringBuilder(projects[0]);
+            foreach (var project in projects.Skip(1))
+            {
+                mruString.Append($"|{project}");
+            }
+            return mruString.ToString();
+        }
+
+        public void AddProject(string project)
+        {
+            if (!Directory.Exists(project)) return;
+
+            if (projects.Contains(project))  // move entry to top
+            {
+                projects.Remove(project);
+                projects.Insert(0, project);
+            }
+            else
+            {
+                projects.Insert(0, project);
+                if (projects.Count > maxProjects)
+                {
+                    projects.RemoveAt(projects.Count - 1);
+                }
+            }
+        }
+
+        public void RemoveProject(string project)
+        {
+            projects.Remove(project);
+        }
+
+        private int maxProjects;
+    }
+}
